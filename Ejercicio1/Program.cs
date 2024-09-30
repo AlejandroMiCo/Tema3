dfef6077bca12cece8e81c29cb5a661678fe3503
Alejandro <alejandromirandaco1801@gmail.com>
2024-09-30 12:30:05 +0200

    caso 0 del ejercicio1

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -8,59 +8,81 @@ using System.Threading.Tasks;
 
 namespace Ejercicio1
 {
-    internal class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
             Dictionary<String, int> ips = new Dictionary<string, int>();
 
-            ips.Add("1111.1111.1111.1111", 100);
-            ips.Add("2222.2222.2222.2222", 200);
-            ips.Add("3333.3333.3333.3333", 300);
-            ips.Add("4444.4444.4444.4444", 400);
-            ips.Add("5555.5555.5555.5555", 500);
+            ips.Add("255.255.255.0", 8);
+            ips.Add("255.2555.0.0", 8);
+            ips.Add("255.0.0.0", 16);
+            ips.Add("255.255.255.16", 16);
 
-                int opcion = 0;
-                int seleccion = -1;
-                string[] opciones = { "Introducir Datos", "Eliminar Dato", "Muestra Coleccion", "Mostrar Elemento", "Salir" };
-                Console.CursorVisible = false;
-                pintaMenu(opciones, opcion);
-                do
+            int opcion = 0;
+            int seleccion = -1;
+            string[] opciones =
+            {
+                "Introducir Datos",
+                "Eliminar Dato",
+                "Muestra Coleccion",
+                "Mostrar Elemento",
+                "Salir",
+            };
+            Console.CursorVisible = false;
+            pintaMenu(opciones, opcion);
+            do
+            {
+                ConsoleKeyInfo tecla = Console.ReadKey();
+                switch (tecla.Key)
                 {
-                    ConsoleKeyInfo tecla = Console.ReadKey();
-                    switch (tecla.Key)
-                    {
-                        case ConsoleKey.DownArrow:
-                            opcion = opcion < opciones.Length - 1 ? opcion + 1 : opcion;
-                            break;
-                        case ConsoleKey.UpArrow:
-                            opcion = opcion > 0 ? opcion - 1 : opcion;
-                            break;
-                        case ConsoleKey.Spacebar:
-                            seleccion = opcion;
-                        switch (opcion)
-                        {
-                            case 0:
-                                pedirDatos(ips[1]);
-
-                                break;
-
-                            default:
-                                break;
-                        }
+                    case ConsoleKey.DownArrow:
+                        opcion = opcion < opciones.Length - 1 ? opcion + 1 : opcion;
                         break;
-                        case ConsoleKey.Escape:
-                            seleccion = opciones.Length - 1;
-                            break;
+                    case ConsoleKey.UpArrow:
+                        opcion = opcion > 0 ? opcion - 1 : opcion;
+                        break;
+                    case ConsoleKey.Spacebar:
+                        seleccion = opcion;
+                        do
+                        {
+                            switch (opcion)
+                            {
+                                case 0:
+                                    int ram;
+                                    string ip;
+                                    pedirDatos(out ram, out ip);
+                                    ips.Add(ip, ram);
 
-                    }
-                    pintaMenu(opciones, opcion);
-                    Console.WriteLine("Opción elegida: {0}", seleccion);
+                                    break;
+                                case 1:
 
-                } while (seleccion != opciones.Length - 1); // Última opción Salir
+                                    break;
 
-            
+                                case 2:
+
+                                    break;
+
+                                case 3:
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Hasta pronto!!");
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        } while (opcion != 4);
+                        break;
+                    case ConsoleKey.Escape:
+                        seleccion = opciones.Length - 1;
+                        break;
+                }
+                pintaMenu(opciones, opcion);
+                Console.WriteLine("Opción elegida: {0}", seleccion);
+            } while (seleccion != opciones.Length - 1); // Última opción Salir
         }
+
         static void pintaMenu(string[] opciones, int opcion)
         {
             string titulo = "MENU";
@@ -78,59 +100,68 @@ namespace Ejercicio1
                 Console.WriteLine(opciones[i]);
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.ForegroundColor = ConsoleColor.White;
-
             }
-
         }
 
-        static bool pedirDatos(DictionaryEntry dato)
+        public static void pedirDatos(out int ram, out string ip)
         {
-
             bool isInt;
-
-            int ram;
             int number;
             bool isValidNumber;
-            string cadenaAComprobar = dato.Key.ToString();
-            string[] partes = cadenaAComprobar.Split('.');
+            bool isValidIp = true;
+            bool isValidRam = true;
 
-            isInt = int.TryParse(dato.Value.ToString(), out ram);
+            do
+            {
+                Console.WriteLine("introduzca la ip del dispositivo:");
+               ip = Console.ReadLine();
+                string[] partes = ip.Split('.');
 
-            if (!isInt)
-            {
-                Console.WriteLine("El valor introducido no es un entero");
-                return false;
-            }
-
-            if (ram < 0)
-            {
-                Console.WriteLine("El valor de la ram no puede ser negativo");
-                return false;
-            }
-            if (partes.Length != 4)
-            {
-                Console.WriteLine("La Ip no contiene los valores necesarios");
-            }
-            foreach (string parte in partes)
-            {
-                isValidNumber = int.TryParse(parte, out number);
-
-                if (!isValidNumber)
+                if (partes.Length != 4)
                 {
-                    Console.WriteLine("Alguno de lo datos introducidos no es un entero");
-                    return false;
+                    Console.WriteLine("La Ip no contiene los valores necesarios");
+                    isValidIp = false;
+                }
+                else
+                {
+                    foreach (string parte in partes)
+                    {
+                        isValidNumber = int.TryParse(parte, out number);
+
+                        if (!isValidNumber)
+                        {
+                            Console.WriteLine("Alguno de lo datos introducidos no es un entero");
+                            isValidIp = false;
+                        }
+                        if (number < 0 || number > 255)
+                        {
+                            Console.WriteLine(
+                                "Alguno de los datos introducidos no corresponde con los valores de una IPs"
+                            );
+                            isValidIp = false;
+                        }
+                    }
+                }
+            } while (!isValidIp);
+
+
+            do
+            {
+                Console.WriteLine("introduzca la cantidad de ram del dispositivo:");
+                isInt = int.TryParse(Console.ReadLine(), out ram);
+
+                if (!isInt)
+                {
+                    Console.WriteLine("El valor introducido no es un entero");
+                    isValidRam = false;
                 }
 
-                if (number < 0 || number > 255)
+                if (ram < 0)
                 {
-                    Console.WriteLine("Alguno de los datos introducidos no corresponde con los valores de una IPs");
-                    return false;
+                    Console.WriteLine("El valor de la ram no puede ser negativo");
+                    isValidRam = false;
                 }
-            }
-
-
-
-            return true;
+            } while (!isValidRam);
         }
     }
 }
